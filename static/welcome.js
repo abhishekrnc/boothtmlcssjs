e5a76ccbbee175ef4e6dc324576d9f8c6b63efed
Abhishek <abhishektupudana@gmail.com>
2021-02-16 19:10:04 +0530

    updated

--- static/welcome.js
+++ static/welcome.js
@@ -4,3 +4,16 @@ function myfun(x,y) {
 
 document.write(myfun(12,13))
 alert("Welcome")
+
+function myfun2() {
+    var txt
+    if(confirm('press a button')) {
+        txt= 'you pressed ok'
+    }
+    else{
+        txt='its got cancel'
+    }
+    document.getElementById('result').innerHTML=txt
+}
+
+ //document.getElementById('result').innerHTML=myfun(12,13)
