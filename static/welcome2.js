36c180849069d1896aa1e54fc6a3c4ce392db82d
Abhishek <abhishektupudana@gmail.com>
2021-02-18 23:08:10 +0530

    updated

--- static/welcome2.js
+++ static/welcome2.js
@@ -3,7 +3,8 @@ function validate() {
     if(document.myform.name.value == "") {
 
         alert("enter your name")
+        
         document.myform.name.focus()
-        return
+        return false
     }
 }
